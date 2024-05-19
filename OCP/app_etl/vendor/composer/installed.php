b50b6e0e7e622456fac69cbcc3b9baa542b76ab3
José Weliton Silva dos Santos <welitonkira2gmail.com>
2024-05-19 13:20:38 -0300

    iniciado o projeto app_etl

---
+++ OCP/app_etl/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'weliton/app_etl',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2d3f2845795ebcc31cc5b28ea168963ebe06ae56',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'weliton/app_etl' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2d3f2845795ebcc31cc5b28ea168963ebe06ae56',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
